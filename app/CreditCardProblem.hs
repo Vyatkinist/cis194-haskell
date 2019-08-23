ec219deea15082b538ab8c2e415ea49ffc2b780b
VV <vyatkinvladislav@gmail.com>
2019-08-23 13:58:45 +0300

    sumDigits.

--- app/CreditCardProblem.hs
+++ app/CreditCardProblem.hs
@@ -4,33 +4,47 @@
 
 module CreditCard where
 
-toDigits :: Int -> [Int]
+toDigits :: Integer -> [Integer]
 toDigits n 
     | n > 0 = (toDigits (n `div` 10)) ++ [n `mod` 10]
     | otherwise = []
 
-reverseList :: [Int] -> [Int]
+reverseList :: [Integer] -> [Integer]
 reverseList [] = []
 reverseList (x:xs) = reverseList(xs) ++ [x]
 
-toDigitsRev :: Int -> [Int]
+toDigitsRev :: Integer -> [Integer]
 toDigitsRev n = reverseList (toDigits n)
 
-arrToString :: [Int] -> [Char]
+arrToString :: [Integer] -> [Char]
+arrToString [] = ""
 arrToString (x:[]) = show x
 arrToString (x:xs) = show x ++ ", " ++ (arrToString xs)
 
-doubleEveryOther :: [Int] -> [Int]
+doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther (reverseList -> (x:y:zs)) = doubleEveryOther (reverse zs) ++ [y*2, x]
 doubleEveryOther (x:[]) = [x]
 doubleEveryOther _ = []
 
+sumDigits :: [Integer] -> Integer
+sumDigits (x:[]) = x
+sumDigits (intArrToDigitsArr -> x:xs) = x + sumDigits(xs)
+
+intArrToDigitsArr :: [Integer] -> [Integer]
+intArrToDigitsArr (x:[]) = toDigits x ++ []
+intArrToDigitsArr (x:xs) = (toDigits x) ++ (intArrToDigitsArr xs)
+
+
+
+a, b, c, d, e, f :: Bool
+g :: Bool
 a = toDigits 1234 == [1,2,3,4]
 b = toDigitsRev 1234 == [4,3,2,1]
 c = toDigits 0 == []
 d = toDigits (-17) == []
 e = doubleEveryOther [8,7,6,5] == [16,7,12,5]
 f = doubleEveryOther [1,2,3] == [1,4,3]
+g = sumDigits [16,7,12,5] == 22
 
 main :: IO ()
 main = do
@@ -40,5 +54,4 @@ main = do
     putStrLn (show d)
     putStrLn (show e)
     putStrLn (show f)
-
-    putStrLn(arrToString(doubleEveryOther [1, 2, 3]))
+    putStrLn (show g)
