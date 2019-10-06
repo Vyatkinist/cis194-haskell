24c1b442edec75eea53c429a1af8b01accae9e7e
VV <vyatkinvladislav@gmail.com>
2019-10-06 22:04:42 +0300

    Homework 3 exercise 3.

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -3,6 +3,8 @@
 {-# LANGUAGE ViewPatterns #-}
 
 module Golf where
+import Data.Tuple
+import Data.List
 
 skips :: [a] -> [[a]]
 skips xs = map (\n -> everyNth n xs) [1..(length xs)]
@@ -12,7 +14,27 @@ skips xs = map (\n -> everyNth n xs) [1..(length xs)]
     where everyNth n xs = map (\(el, index) -> el) $ filter (\(el, index) -> index == n) $ zip xs $ cycle [1..n]
 
 localMaxima :: [Integer] -> [Integer]
+-- Create triplets with offset 1 from array, filter ones with middle element to be the largest
 localMaxima xs = map (\(a, b, c) -> b) $ filter (\(a, b, c) -> b > c && b > a) $ zip3 xs (drop 1 xs) (drop 2 xs)
+
+histogram :: [Integer] -> String
+histogram xs = reverse . unlines $ legend ++ map (\row -> histString row $ pairs xs) [1..maxRow]
+    where
+        -- Create an array of '=0', '=1' etc, and then transpose
+        legend = transpose . reverse $ map (\n -> "=" ++ show n) [0..9]
+        -- Gets the maximum column height
+        maxRow = maximum $ snd <$> pairs xs
+        -- Make a list of 0..9, get counts for every number
+        pairs xs = map (\n -> (n, countTimes n xs)) [0..9]
+            where 
+                -- Count how many times n occurs in a list
+                countTimes n xs = sum $ map (\x -> 1) $ filter (\x -> x == n) xs
+        -- Get a single row of a histogram        
+        histString row xs = reverse $ map (\(index, count) -> numToChar row count) xs
+            where
+                numToChar row c
+                    | c < row   = ' '
+                    | c >= row  = '*'
 
 main :: IO()
 main = do
@@ -26,4 +48,7 @@ main = do
     putStrLn . show $ localMaxima [2,9,5,6,1] == [9,6]
     putStrLn . show $ localMaxima [2,3,4,1,5] == [4]
     putStrLn . show $ localMaxima [1,2,3,4,5] == []
-    
+
+    putStrLn "Exercise 3:"
+    putStrLn $ histogram [1,4,5,4,6,6,3,4,2,4,9]
+    putStrLn $ histogram [1,1,1,5]
